91ef3bd464ae8761c1c7b453fabfe2c27b28d92d
Spencer Campbell <119441077+spencercampbell42@users.noreply.github.com>
2024-04-19 14:49:13 -1000

    feat: support custom icon

--- src/VueTailwindDatePicker.vue
+++ src/VueTailwindDatePicker.vue
@@ -1302,7 +1302,8 @@ provide('setToCustomShortcut', setToCustomShortcut)
           @keyup="keyUp"
         />
         <div class="absolute inset-y-0 right-0 inline-flex items-center rounded-md overflow-hidden">
-          <button
+          <slot name="icon" />
+          <!-- <button
             type="button"
             class="px-2 py-1 mr-1 focus:outline-none text-vtd-secondary-400 dark:text-opacity-70 rounded-md"
             @click="pickerValue ? clearPicker() : $refs.VtdInputRef.focus()"
@@ -1329,7 +1330,7 @@ provide('setToCustomShortcut', setToCustomShortcut)
                 d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
               ></path>
             </svg>
-          </button>
+          </button> -->
         </div>
       </slot>
     </PopoverButton>
